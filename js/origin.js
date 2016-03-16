f8bcb5be0f4b1fef0f8c6ad3c8ea016bf5015720
Jan Hruby <jan.hruby@pioneers.io>
2016-03-17 00:11:50 +0100

    initial commit

---
+++ js/origin.js
@@ -0,0 +1,77 @@
+// =============================================================================
+// Origin.js
+// =============================================================================
+
+//
+// Collection of scripts used by the framework
+//
+
+// =============================================================================
+// smoothScroll
+// =============================================================================
+
+$('a').click(function() {
+	$('html, body').animate({
+	  scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+	}, 500);
+	return false;
+});
+
+// =============================================================================
+// hidingNavbar
+// =============================================================================
+
+$(function(){
+	var lastScrollTop = 0, delta = 50;
+	var bottom = $(document).height() - $(window).height();
+	$(window).scroll(function (event) {
+	var scrollTop = $(this).scrollTop();
+
+	if(Math.abs(lastScrollTop - scrollTop) <= delta)
+	  return;
+
+	if (scrollTop > lastScrollTop) {
+	   // downscroll code
+	  $('.navbar').addClass('navbar--hidden');
+	} else {
+	  // upscroll code
+	  $('.navbar').removeClass('navbar--hidden');
+	} 
+	if (scrollTop > bottom - delta) {
+	  // end of page code
+	  $('.navbar').removeClass('navbar--hidden');
+	}
+	if (scrollTop <= 0) {
+		// top of page code
+		$('.navbar').removeClass('navbar--hidden');
+	}
+	lastScrollTop = scrollTop;
+	});
+});
+
+// =============================================================================
+// Show mobile navbar
+// =============================================================================
+$(function(){
+	$(".navbar .navigation-open").removeClass("navigation-open");
+	$(".nav__link--mobiletoggle").click(function () {
+		$(".navbar .nav").toggleClass("navigation-open");
+	});
+});
+
+// =============================================================================
+// Remove navbar notification
+// =============================================================================
+
+$(function() {
+	$('.nav__link').click(function() {
+		$(this).next().hide(); // Careful, dependent on DOM structure
+	});
+});
+
+// =============================================================================
+// ScrollReveal settings
+// =============================================================================
+
+window.sr = ScrollReveal({ reset: true, duration: 1000, mobile: false });
+    sr.reveal( '.animate' );
