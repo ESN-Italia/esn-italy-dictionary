6be4f7d5b47d9fd5ccc3c1c3188c87b230d49952
MC <mc@matteocarbone.com>
2019-03-22 13:12:17 +0100

    init ESN Italy

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# origin
+# ESN Italy's dictionary
+
+[Here you can find the Google Sheet with all the terms](https://docs.google.com/spreadsheets/d/1iWZMs2t-Nf3EChVqhkBRN3xRJuqkTUYuebyD0tRKrYc).
+
+Forked from [https://github.com/mrozilla/esn-dictionary](https://github.com/mrozilla/esn-dictionary).
--- js/interaction.js
+++ js/interaction.js
@@ -1,26 +1,28 @@
 $(function () {
-  // ID of the Google Spreadsheet
-  //https://docs.google.com/spreadsheets/d/10z3kwGXRzjqd2Y3V35cyFyMAIQ-R-SEk0FNOIfI0zis/pubhtml
-  var spreadsheetID = "10z3kwGXRzjqd2Y3V35cyFyMAIQ-R-SEk0FNOIfI0zis";
+  // ID of the Google Spreadsheet (@mc is in the CIA folder)
+  var spreadsheetID = "1iWZMs2t-Nf3EChVqhkBRN3xRJuqkTUYuebyD0tRKrYc";
 
   // Make sure it is public or set to Anyone with link can view 
-  var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
+  var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/default/public/values?alt=json";
 
   var cardTemplate = $('#dictionary__entry').html();
 
   $.getJSON(url, function(result) {
 
     var entries = result.feed.entry.map( function (entry) {
-          
+
       var card = $($('<div>').html(cardTemplate).children()[0]);
 
-      card.find('.dictionary__phrase').text(entry.gsx$phrase.$t);
-      card.find('.dictionary__type').text(entry.gsx$type.$t);
-      card.find('.dictionary__grammar').text(entry.gsx$grammar.$t);
-      card.find('.dictionary__description').text(entry.gsx$numbereddescription.$t);
-      card.find('.dictionary__usage').text(entry.gsx$usageexample.$t);
+      card.find('.dictionary__phrase').text(entry.gsx$termine.$t);
+      card.find('.dictionary__type').text(entry.gsx$categoria.$t);
+      card.find('.dictionary__grammar').text(entry.gsx$grammatica.$t);
+      // @mc enable multiple definitions
+      entry.gsx$definizione.$t.split('||').forEach(d => {
+        card.find('.dictionary__description').append(`<li>${d}</li>`);
+      });
+      card.find('.dictionary__usage').text(entry.gsx$esempio.$t);
 
-      entry.gsx$anchor.$t != "" ? card.find('.dictionary__anchor').attr("name", entry.gsx$anchor.$t.toLowerCase()) : null;
+      entry.gsx$ancora.$t != "" ? card.find('.dictionary__anchor').attr("name", entry.gsx$ancora.$t.toLowerCase()) : null;
 
       return card;
 
