fa360f3926e2781eb475752072a5e9b738b656d5
Matteo Carbone <mc@matteocarbone.com>
2022-07-11 20:06:57 +0200

    fix: alternative to deprecated Google Sheets API (v3)

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 [Here you can find the Google Sheet with all the terms](https://docs.google.com/spreadsheets/d/1iWZMs2t-Nf3EChVqhkBRN3xRJuqkTUYuebyD0tRKrYc).
 
-Forked from [https://github.com/mrozilla/esn-dictionary](https://github.com/mrozilla/esn-dictionary).
+After the [deprecation of Google Sheet APIs v3](https://cloud.google.com/blog/products/g-suite/migrate-your-apps-use-latest-sheets-api), we switched towards [this alternative](https://github.com/benborgers/opensheet).
+
+---
+
+_Forked from [https://github.com/mrozilla/esn-dictionary](https://github.com/mrozilla/esn-dictionary)._
--- js/interaction.js
+++ js/interaction.js
@@ -3,29 +3,29 @@ $(function() {
   var spreadsheetID = '1iWZMs2t-Nf3EChVqhkBRN3xRJuqkTUYuebyD0tRKrYc';
 
   // Make sure it is public or set to Anyone with link can view
-  var url = 'https://spreadsheets.google.com/feeds/list/' + spreadsheetID + '/default/public/values?alt=json';
+  var url = 'https://opensheet.elk.sh/' + spreadsheetID + '/1';
 
   var cardTemplate = $('#dictionary__entry').html();
 
   $.getJSON(url, function(result) {
-    var entries = result.feed.entry.map(function(entry) {
+    var entries = result.map(function(entry) {
+      console.log(entry);
       var card = $(
         $('<div>')
           .html(cardTemplate)
           .children()[0]
       );
 
-      card.find('.dictionary__phrase').text(entry.gsx$termine.$t);
-      card.find('.dictionary__type').text(entry.gsx$categoria.$t);
-      card.find('.dictionary__grammar').text(entry.gsx$pronuncia.$t);
-      // @mc enable multiple definitions
-      entry.gsx$definizione.$t.split('||').forEach(d => {
+      card.find('.dictionary__phrase').text(entry.Termine);
+      card.find('.dictionary__type').text(entry.Categoria);
+      card.find('.dictionary__grammar').text(entry.Pronuncia);
+      entry.Definizione.split('||').forEach(d => {
         card.find('.dictionary__description').append(`<li>${d}</li>`);
       });
-      card.find('.dictionary__usage').text(entry.gsx$esempio.$t);
+      card.find('.dictionary__usage').text(entry.Esempio);
 
-      entry.gsx$ancora.$t != ''
-        ? card.find('.dictionary__anchor').attr('name', entry.gsx$ancora.$t.toLowerCase())
+      entry.Ancora
+        ? card.find('.dictionary__anchor').attr('name', entry.Ancora.toLowerCase())
         : null;
 
       return card;
